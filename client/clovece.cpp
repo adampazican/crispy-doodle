da1ebaa0de9cbc99d900e3e5e4d147d6edfcc5f9
Trsten <p.trstensky@gmail.com>
2020-01-03 21:25:35 +0100

    added game for 2,3,4 playes and changed drawing

--- client/clovece.cpp
+++ client/clovece.cpp
@@ -4,7 +4,6 @@
 #include "../common.h"
 
 #define FIGURES_FOR_PLAYERS 4
-#define NUMBER_OF_PLAYERS 4
 #define HOUSE_SIZE 4
 #define GAME_SIZE 32
 
@@ -21,6 +20,9 @@ struct Coordinates
 {
     int x,y;
 };
+
+Square squares[11][11];
+int numberOfPlayers;
 
 Coordinates getSquareInFinish(int possition, int playerId)
 {
@@ -154,10 +156,46 @@ Coordinates getSquareInGame(int possition,int playerId) {
 	return coordinates;
 }
 
-
-
-void vykresli(Square squeres[11][11]) 
+Color getColorByTurnId(int id)
 {
+	if (id == 0)
+		return Color::RED;
+	else if (id == 1)
+		return Color::GREEN;
+	else if (id == 2)
+		return Color::YELLOW;
+	else
+		return Color::BLUE;
+}
+
+void draw(Game game) 
+{
+	for (int  i = 0; i < numberOfPlayers; i++)
+	{
+		for (int j = 0; j < FIGURES_FOR_PLAYERS; j++)
+		{
+			FigureState state = game.players[i].figurines[j].figurineState;
+			int possition = game.players[i].figurines[j].position;
+			Color color = getColorByTurnId(i);
+			struct Coordinates poss;
+
+			switch (state)
+			{
+				case FigureState::IN_HOUSE:
+					poss = getSquareInHouse(possition,i);
+					break;
+				case FigureState::IN_GAME:
+					poss = getSquareInGame(possition,i);
+					break;
+				case FigureState::IN_FINISH:
+					poss = getSquareInFinish(possition,i);
+					break;
+			}
+			squares[poss.y][poss.x].character = '0' + (j + 1);
+			squares[poss.y][poss.x].color = color;
+		}		
+	}
+	
 	for (int i = 0; i < 11; i++)
 	{
 		if (i == 0)
@@ -172,33 +210,23 @@ void vykresli(Square squeres[11][11])
         
 		for (int j = 0; j < 11; j++)
 		{
-			/*			foreground background
-    black        30         40
-    red          31         41
-    green        32         42
-    yellow       33         43
-    blue         34         44
-    magenta      35         45
-    cyan         36         46
-    white        37         47
-    */
 			cout << ' ';
-			switch (squeres[i][j].color)
+			switch (squares[i][j].color)
 			{
                 case Color::BLUE:
-				cout << "\033[1;34m"<< squeres[i][j].character  << "\033[0m";
+				cout << "\033[1;34m"<< squares[i][j].character  << "\033[0m";
 				break;
                 case Color::GREEN:
-				cout << "\033[1;32m" << squeres[i][j].character << "\033[0m";
+				cout << "\033[1;32m" << squares[i][j].character << "\033[0m";
 				break;
                 case Color::YELLOW:
-				cout << "\033[1;33m" << squeres[i][j].character << "\033[0m";
+				cout << "\033[1;33m" << squares[i][j].character << "\033[0m";
 				break;
                 case Color::RED:
-				cout << "\033[1;31m" << squeres[i][j].character << "\033[0m";
+				cout << "\033[1;31m" << squares[i][j].character << "\033[0m";
 				break;
                 case Color::WHITE:
-				cout << squeres[i][j].character;
+				cout << squares[i][j].character;
 				break;
 			}
 		}
@@ -330,45 +358,23 @@ bool allInFinish(Player player)
 	return number == 4 ? true : false;
 }
 
-void PressEnterToContinue()
+void PressEnterToContinue(int number)
 {
+
 	int c;
-	printf("Press ENTER to roll. ");
+	if (number == 0)
+		printf("Press ENTER to roll. ");
+	else if (number == 1)
+		printf("Press ENTER to contunie in turn.");
+	else 
+		printf("Press ENTER to finsih turn.");
 	fflush(stdout);
 	do c = getchar(); while ((c != '\n') && (c != EOF));
 }
 
-void PressEnterToEnd()
-{
-	int c;
-	printf("Press ENTER to finsih turn.");
-	fflush(stdout);
-	do c = getchar(); while ((c != '\n') && (c != EOF));
-}
+void clearBoard() {
 
-Color getColorByTurnId(int id)
-{
-	if (id == 0)
-		return Color::RED;
-	else if (id == 1)
-		return Color::GREEN;
-	else if (id == 2)
-		return Color::YELLOW;
-	else
-		return Color::BLUE;
-}
-
-int main()
-{
-	srand(time(NULL));
-    
-	Game game;
-	game.gameState = GameState::PLAYING;
-	game.turnId = 0;
-    
-	Square squeres[11][11];
-    
-	for (int  i = 0; i < 11; i++)
+for (int  i = 0; i < 11; i++)
 	{
 		for (int  j = 0; j < 11; j++)
 		{
@@ -377,79 +383,90 @@ int main()
 			{
 				if (i == 5 && j == 5)
 				{
-					squeres[i][j].character = ' ';
-					squeres[i][j].color = Color::WHITE;
+					squares[i][j].character = ' ';
+					squares[i][j].color = Color::WHITE;
 				}
 				else
 				{
-					squeres[i][j].character = 'O';
-					squeres[i][j].color = Color::WHITE;
+					squares[i][j].character = 'O';
+					squares[i][j].color = Color::WHITE;
 				}
 			}
 			else if (j > 3 && j < 7)				//Hracie policka
 			{
-				squeres[i][j].character = 'O';
-				squeres[i][j].color = Color::WHITE;
+				squares[i][j].character = 'O';
+				squares[i][j].color = Color::WHITE;
 			}
 			else if (i < 2 && j < 2)				//IN_HOUSE_POLICKA
 			{
-				squeres[i][j].character = 'O';
-				squeres[i][j].color = Color::RED;
+				squares[i][j].character = 'O';
+				squares[i][j].color = Color::RED;
 			}
 			else if (i > 8 && j < 2)
 			{
-				squeres[i][j].character = 'O';
-				squeres[i][j].color = Color::BLUE;
+				squares[i][j].character = 'O';
+				squares[i][j].color = Color::BLUE;
 			}
 			else if (i < 2 && j >  8)
 			{
-				squeres[i][j].character = 'O';
-				squeres[i][j].color = Color::GREEN;
+				squares[i][j].character = 'O';
+				squares[i][j].color = Color::GREEN;
 			}
 			else if (i > 8 && j > 8)
 			{
-				squeres[i][j].character = 'O';
-				squeres[i][j].color = Color::YELLOW;
+				squares[i][j].character = 'O';
+				squares[i][j].color = Color::YELLOW;
 			}
 			else			// prazdne policka
 			{
-				squeres[i][j].character = ' ';
-				squeres[i][j].color = Color::WHITE;
+				squares[i][j].character = ' ';
+				squares[i][j].color = Color::WHITE;
 			}
-            
-			if (i == 5 && j > 0 && j < 5)				//INFINISH policka
-				squeres[i][j].color = Color::BLUE;
+            //INFINISH policka
+			if (i == 5 && j > 0 && j < 5)				
+				squares[i][j].color = Color::BLUE;
 			if (i == 5 && j > 5 && j < 10)
-				squeres[i][j].color = Color::GREEN;
+				squares[i][j].color = Color::GREEN;
 			if (j == 5 && i > 0 && i < 5)
-				squeres[i][j].color = Color::RED;
+				squares[i][j].color = Color::RED;
 			if (j == 5 && i > 5 && i < 10)
-				squeres[i][j].color = Color::YELLOW;
+				squares[i][j].color = Color::YELLOW;
             
 			if (j == 4 && i == 0)
 			{
-				squeres[i][j].color = Color::RED;
-				squeres[i][j].character = '0';
+				squares[i][j].color = Color::RED;
+				squares[i][j].character = '0';
 			}
 			if (j == 0 && i == 6)
 			{
-				squeres[i][j].color = Color::BLUE;
-				squeres[i][j].character = '0';
+				squares[i][j].color = Color::BLUE;
+				squares[i][j].character = '0';
 			}
 			if (j == 10 && i == 4)
 			{
-				squeres[i][j].color = Color::GREEN;
-				squeres[i][j].character = '0';
+				squares[i][j].color = Color::GREEN;
+				squares[i][j].character = '0';
 			}
 			if (j == 6 && i == 10)
 			{
-				squeres[i][j].color = Color::YELLOW;
-				squeres[i][j].character = '0';
+				squares[i][j].color = Color::YELLOW;
+				squares[i][j].character = '0';
 			}
 		}
 	}
+}
+
+int main()
+{
+	srand(time(NULL));
     
-	for (int i = 0; i <  FIGURES_FOR_PLAYERS ; i++)
+	numberOfPlayers = 2;
+
+	struct Game game;
+	game.gameState = GameState::PLAYING;
+	game.turnId = 0;
+    
+	for (int i = 0; i <  numberOfPlayers ; i++)
 	{
 		game.players[i].playerId = i;
         
@@ -457,19 +474,16 @@ int main()
 		{
 			game.players[i].figurines[j].figurineState = FigureState::IN_HOUSE;
 			game.players[i].figurines[j].position = j + 1;
-			
-			Coordinates suradnice = getSquareInHouse(j + 1, i);
-			squeres[suradnice.y][suradnice.x].character = '0' + game.players[i].figurines[j].position;
 		}
 	}
-    
+
 	game.turnId = 0;
     
 	int umiestnenie[4];
 	bool ukoncene[4];
 	int index = 0;
     
-	for (int  i = 0; i < NUMBER_OF_PLAYERS; i++)
+	for (int  i = 0; i < numberOfPlayers; i++)
 	{
 		umiestnenie[i] = -1;
 		ukoncene[i] = false;
@@ -478,9 +492,10 @@ int main()
 	while (game.gameState != GameState::FINISHED)
 	{
 		system("clear");
-		vykresli(squeres);
+		clearBoard();
+		draw(game);
         
-		game.turnId %= 4;
+		game.turnId %= numberOfPlayers;
         
 		if (game.turnId == 0)
 			cout << "\033[1;31m" << "Red on the turn" << "\033[0m" << endl;
@@ -500,7 +515,7 @@ int main()
 		{
 			for (int i = 0; i < 3; i++)
 			{
-				PressEnterToContinue();
+				PressEnterToContinue(0);
 				number = getNumber();
 				cout << "You got number " << number << endl;
 				
@@ -518,7 +533,7 @@ int main()
 			number = 0;
 		}
 		else {		//ak nema vsetkych v domceku hadze iba raz
-			PressEnterToContinue();
+			PressEnterToContinue(0);
 			number = getNumber();
 			cout << "You got number " << number << endl;
 			if (number == 6)
@@ -527,15 +542,30 @@ int main()
 		//ak hodi sestku hadze znova
 		while (number == 6)
 		{
-			PressEnterToContinue();
+			PressEnterToContinue(0);
 			number = getNumber();
 			cout << "You got number " << number << endl;
 			if (number == 6)
 				numberOfSix++;
 		}
-        
+
+		PressEnterToContinue(1);
+
 		while (numberOfSix >= 0)
 		{
+			system("clear");
+			clearBoard();
+			draw(game);
+
+			if (game.turnId == 0)
+				cout << "\033[1;31m" << "Red on the turn" << "\033[0m" << endl;
+			else if (game.turnId == 1)
+				cout << "\033[1;32m" << "Green on the turn." << "\033[0m" << endl;
+			else if (game.turnId == 2)
+				cout << "\033[1;33m" << "Yellow on the turn." << "\033[0m" << endl;
+			else if (game.turnId == 3)
+				cout << "\033[1;34m" << "Blue on the turn." << "\033[0m" << endl;
+
 			int maxNumber = 0;
             
 			if (numberOfSix > 0)
@@ -545,15 +575,18 @@ int main()
             
 			if (allInHouse(game.players[game.turnId]) && maxNumber != 6)
 			{
-				PressEnterToEnd();
+				cout << "you cant move." << endl;
+				PressEnterToContinue(1);
 			}
 			else if (allInFinish(game.players[game.turnId]))
 			{
-				PressEnterToEnd();
+				cout << "you cant move." << endl;
+				PressEnterToContinue(1);
 			}
 			else if ( numberOfplayable(game.players[game.turnId],maxNumber) == 0 )
 			{
-				PressEnterToEnd();
+				cout << "you cant move." << endl;
+				PressEnterToContinue(1);
 			}
 			else 
 			{
@@ -573,114 +606,63 @@ int main()
 				//ak je v domceku a hodim 6 tak ho pripravim na start
 				if (game.players[game.turnId].figurines[figNumber].figurineState == FigureState::IN_HOUSE && maxNumber == 6)
 				{
-					Coordinates poss = getSquareInHouse(game.players[game.turnId].figurines[figNumber].position, game.turnId);
-					squeres[poss.y][poss.x].character = '0';
-                    
 					game.players[game.turnId].figurines[figNumber].position = 0;
 					game.players[game.turnId].figurines[figNumber].figurineState = FigureState::IN_GAME;
-                    
-					poss = getSquareInGame(game.players[game.turnId].figurines[figNumber].position, game.turnId);
-					squeres[poss.y][poss.x].character = '0' + (figNumber + 1);
-                    
-					system("clear");
-					vykresli(squeres);
-                    
 				}
 				else if (game.players[game.turnId].figurines[figNumber].figurineState == FigureState::IN_GAME)
 				{
-					//vyprazdni policko
-					Coordinates poss = getSquareInGame(game.players[game.turnId].figurines[figNumber].position, game.turnId);
-                    
-					squeres[poss.y][poss.x].character = 'O';
-					squeres[poss.y][poss.x].color = Color::WHITE;
-                    
-					//ak je to pociatocne policko nemeni farbu
-					if (game.players[game.turnId].figurines[figNumber].position == 0)
-					{
-						int figInStart = 0;
-						for (int i = 0; i < HOUSE_SIZE; i++)
-						{
-							if (game.players[game.turnId].figurines[i].position == 0 && figNumber != i) {
-								figInStart = i + 1;
-							}
-						}
-                        
-						squeres[poss.y][poss.x].character = '0' + figInStart;
-						squeres[poss.y][poss.x].color = getColorByTurnId(game.turnId);
-                        
-					}
-                    
 					//overenie ci nejdem už do domceka
 					if (game.players[game.turnId].figurines[figNumber].position + maxNumber > 37)
 					{
 						//presunie do ciela
 						game.players[game.turnId].figurines[figNumber].figurineState = FigureState::IN_FINISH;
 						game.players[game.turnId].figurines[figNumber].position += maxNumber - 37;
-						poss = getSquareInFinish(game.players[game.turnId].figurines[figNumber].position, game.turnId);
-						squeres[poss.y][poss.x].character = '0' + figNumber + 1;
 					}
 					else
 					{
-						//posunie na nove miesto na hracej ploche
-						poss = getSquareInGame(game.players[game.turnId].figurines[figNumber].position + maxNumber, game.turnId);
 						game.players[game.turnId].figurines[figNumber].position += maxNumber;
-						squeres[poss.y][poss.x].character = '0' + figNumber + 1;
-						squeres[poss.y][poss.x].color = getColorByTurnId(game.turnId);
-					}
-					
-                    
-					//overenie ci som nevykopol figurku
-                    
-					for (int i = 0; i < NUMBER_OF_PLAYERS; i++)
-					{
-						for (int j = 0; j < HOUSE_SIZE; j++)
+
+						//overenie ci som nevykopol figurku  
+						for (int i = 0; i < numberOfPlayers; i++)
 						{
-							if (game.turnId != i && game.players[i].figurines[j].figurineState == FigureState::IN_GAME)
+							for (int j = 0; j < FIGURES_FOR_PLAYERS; j++)
 							{
-								Coordinates pom1 = getSquareInGame(game.players[i].figurines[j].position, i);
-                                
-								if (poss.x == pom1.x && poss.y == pom1.y)
+								if (game.turnId != i && game.players[i].figurines[j].figurineState == FigureState::IN_GAME)
 								{
-									cout << "vykopol si figurku" << endl;
-									game.players[i].figurines[j].position = j + 1;
-									game.players[i].figurines[j].figurineState = FigureState::IN_HOUSE;
-									pom1 = getSquareInHouse(game.players[i].figurines[j].position, i);
-									squeres[pom1.y][pom1.x].character = '0' + (j + 1);
+									Coordinates poss = getSquareInGame(game.players[game.turnId].figurines[figNumber].position, game.turnId);
+									Coordinates pom1 = getSquareInGame(game.players[i].figurines[j].position, i);
+
+									if (poss.x == pom1.x && poss.y == pom1.y)
+									{
+										game.players[i].figurines[j].position = j + 1;
+										game.players[i].figurines[j].figurineState = FigureState::IN_HOUSE;
+									}
 								}
 							}
 						}
 					}
-                    
-					system("clear");
-					vykresli(squeres);
-					PressEnterToEnd();
-                    
 				}
 				else if (game.players[game.turnId].figurines[figNumber].figurineState == FigureState::IN_FINISH)
-				{
-					Coordinates poss = getSquareInFinish(game.players[game.turnId].figurines[figNumber].position, game.turnId);
-					squeres[poss.y][poss.x].character = 'O';
-                    
-					poss = getSquareInFinish(game.players[game.turnId].figurines[figNumber].position + maxNumber, game.turnId);
-					squeres[poss.y][poss.x].character = '0' + figNumber + 1;
-                    
-					game.players[game.turnId].figurines[figNumber].position += maxNumber;
+				{   
+					game.players[game.turnId].figurines[figNumber].position += maxNumber;	
 				}
-                
+
+				system("clear");
+				clearBoard();
+				draw(game);
 			}
-            
 			numberOfSix--;
 		}
         
 		int playerFinished = 0;
         
-		for (int i = 0; i < NUMBER_OF_PLAYERS; i++)
+		for (int i = 0; i < numberOfPlayers; i++)
 		{
 			if (allInFinish(game.players[i]))
 				playerFinished++;
 		}
         
-		if (playerFinished == 4)
+		if (playerFinished == numberOfPlayers)
 			game.gameState = GameState::FINISHED;
         
 		if (allInFinish(game.players[game.turnId]))
@@ -693,15 +675,17 @@ int main()
 			}
 		}
         
+		PressEnterToContinue(3);
 		game.turnId++;
 	}
     
 	system("clear");
-	vykresli(squeres);
+	clearBoard();
+	draw(game);
     
-	for (int  i = 0; i < NUMBER_OF_PLAYERS; i++)
+	for (int  i = 0; i < numberOfPlayers; i++)
 	{
-		cout << i << ". miesto: hrac cislo: " << umiestnenie[i] << endl;
+		cout << i + 1 << ". miesto: hrac cislo: " << umiestnenie[i] << endl;
 	}
     
 }
